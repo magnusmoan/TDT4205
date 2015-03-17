227074e3c482d867cd5883d778eb12936bc029e3
magnusmoan <magnus.moan@gmail.com>
2015-03-17 20:59:53 +0100

    .

--- Oving5/src/generator.c
+++ Oving5/src/generator.c
@@ -197,6 +197,8 @@ void gen_EXPRESSION ( node_t *root, int scopedepth )
 			break;
 		case NEW_E:
 			{
+				instruction_add(
+				instruction_add(BL, "_malloc", NULL, 0, 0);
 			}
 			break;
 		case ARRAY_INDEX_E:
