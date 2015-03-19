662380ba9fd87093215e1fd24e6bafe742da55fc
magnusmoan <magnus.moan@gmail.com>
2015-03-19 17:50:14 +0100

    .

--- Oving5/src/generator.c
+++ Oving5/src/generator.c
@@ -202,7 +202,7 @@ void gen_EXPRESSION ( node_t *root, int scopedepth )
 					space *= root->children[0]->data_type.dimensions[i];
 				}
 				char* string[20];
-				printf(string, "%d", index*4);
+				printf(string, "%d", space*4);
 				instruction_add(MOVE32, r6, STRDUP(string), 0, 0);
 				instruction_add(PUSH, r6, NULL, 0, 0);
 				instruction_add(BL, "_malloc", NULL, 0, 0);
