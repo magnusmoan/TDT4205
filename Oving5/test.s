0c601586a2142ca8d23941d5819910ef2a666d7d
magnusmoan <magnus.moan@gmail.com>
2015-03-19 13:16:52 +0100

    .

--- Oving5/test.s
+++ Oving5/test.s
@@ -8,106 +8,35 @@
 .INTEGER: .ascii "%d \000"
 .FLOAT: .ascii "%f \000"
 .NEWLINE: .ascii "\n \000"
+.STRING0: .ascii "Hello, world!"
+.ascii "\000"
 .globl main
 .align	2
 .text
 #0 Starting PROGRAM
-#1 Starting FUNCTION (main) with depth 2
-_main:
+#1 Starting FUNCTION (hello) with depth 2
+_hello:
 	push	{lr}
 	push	{fp}
 	mov	fp, sp
-#2 Starting DECLARATION: adding space on stack
-	push	{r0}
-#3 Ending DECLARATION
-#4 Starting DECLARATION: adding space on stack
-	push	{r0}
-#5 Ending DECLARATION
-#6 Starting ASSIGNMENT_STATEMENT
-#7 Starting VARIABLE
-	ldr	r5, [fp, #-4]
-	push	{r5}
-#8 End VARIABLE a, depth difference: 0, stack offset: -4
-#9 Starting CONSTANT
-	movw	r5, #:lower16:5
-	movt	r5, #:upper16:5
-	push	{r5}
-#10 End CONSTANT
-	pop	{r5}
-	str	r5, [fp, #-4]
-#11 End ASSIGNMENT_STATEMENT
-#12 Starting ASSIGNMENT_STATEMENT
-#13 Starting VARIABLE
-	ldr	r5, [fp, #-8]
-	push	{r5}
-#14 End VARIABLE b, depth difference: 0, stack offset: -8
-#15 Starting EXPRESSION of type FUNC_CALL
-	bl	_func
-#16 Ending EXPRESSION of type FUNC_CALL
-	pop	{r5}
-	str	r5, [fp, #-8]
-#17 End ASSIGNMENT_STATEMENT
-#18 Starting PRINT_STATEMENT
+#2 Starting PRINT_STATEMENT
 	push	{r6}
 	pop	{r6}
-#19 Starting VARIABLE
-	ldr	r5, [fp, #-4]
+#3 Starting CONSTANT
+	movw	r5, #:lower16:.STRING0
+	movt	r5, #:upper16:.STRING0
 	push	{r5}
-#20 End VARIABLE a, depth difference: 0, stack offset: -4
-	movw  r0, #:lower16:.INTEGER
-	movt  r0, #:upper16:.INTEGER
-	pop	{r1}
-	bl	printf
-#21 Starting VARIABLE
-	ldr	r5, [fp, #-8]
-	push	{r5}
-#22 End VARIABLE b, depth difference: 0, stack offset: -8
-	movw  r0, #:lower16:.INTEGER
-	movt  r0, #:upper16:.INTEGER
-	pop	{r1}
-	bl	printf
-	movw	r0, #:lower16:0x0A
-	movt	r0, #:upper16:0x0A
-	bl	putchar
-#23 Ending PRINT_STATEMENT
-	mov	sp, fp
-	pop	{fp}
-	pop	{pc}
-#24 Leaving FUNCTION (main) with depth 2
-#25 Starting FUNCTION (func) with depth 2
-_func:
-	push	{lr}
-	push	{fp}
-	mov	fp, sp
-#26 Starting DECLARATION: adding space on stack
-	push	{r0}
-#27 Ending DECLARATION
-#28 Starting PRINT_STATEMENT
-	push	{r6}
-	pop	{r6}
-#29 Starting VARIABLE
-	ldr	r5, [fp, #8]
-	push	{r5}
-#30 End VARIABLE x, depth difference: 0, stack offset: 8
-	movw  r0, #:lower16:.INTEGER
-	movt  r0, #:upper16:.INTEGER
-	pop	{r1}
-	bl	printf
-	movw	r0, #:lower16:0x0A
-	movt	r0, #:upper16:0x0A
-	bl	putchar
-#31 Ending PRINT_STATEMENT
-#32 Starting RETURN_STATEMENT
-#33 Starting VARIABLE
-	ldr	r5, [fp, #8]
-	push	{r5}
-#34 End VARIABLE x, depth difference: 0, stack offset: 8
+#4 End CONSTANT
 	pop	{r0}
-#35 End RETURN_STATEMENT
+	bl	printf
+	movw	r0, #:lower16:0x0A
+	movt	r0, #:upper16:0x0A
+	bl	putchar
+#5 Ending PRINT_STATEMENT
 	mov	sp, fp
 	pop	{fp}
 	pop	{pc}
-#36 Leaving FUNCTION (func) with depth 2
+#6 Leaving FUNCTION (hello) with depth 2
 debugprint:
 	push {r0-r11, lr}
 	movw	r0, #:lower16:.DEBUG
@@ -148,8 +77,8 @@ pusharg:
 	cmp	r5,#0
 	bne	pusharg
 noargs:
-	bl	_main
-#37 End PROGRAM
+	bl	_hello
+#7 End PROGRAM
 	mov	sp, fp
 	pop	{fp}
 	bl	exit
